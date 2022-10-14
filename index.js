b2b42f0596ec61601848902c6560ab215e8db654
Chaiyapat Oam <83873103+ChaiyapatOam@users.noreply.github.com>
2022-10-14 22:56:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
+var listAnswer = [];
+function AddListAnswer(input) {
+  if (listAnswer.indexOf(input) == -1) listAnswer.push(input);
+
+  const allAnswer = document.getElementById("allAnswer");
+  allAnswer.style.color = 'purple'
+  allAnswer.innerHTML = listAnswer;
+}
 var random = Math.floor(Math.random() * 11);
 function getValue() {
   console.log(random);
   var input = document.getElementById("inputNumber").value;
   if (!input) input = null;
   console.log("input = " + input);
+  AddListAnswer(input);
   const answer = document.getElementById("answer");
   if (input > random) {
     answer.style.color = "red";
